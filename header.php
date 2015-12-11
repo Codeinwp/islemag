51483f56221dcd568f1958a8ada9967e18ca2133
cristian-ungureanu <cristian@themeisle.com>
2015-12-11 15:37:12 +0200

    !!! header changes

--- header.php
+++ header.php
@@ -22,11 +22,9 @@
 
 <body <?php body_class(); ?>>
     <div id="wrapper" class="boxed-long">
-        
+
         <header id="masthead" class="site-header" role="banner">
-            
-            
-            
+
             <div class="navbar-top container-fluid">
                 <div class="navbar-left social-icons">
                     <?php
@@ -49,45 +47,22 @@
                         }
                     ?>
                 </div>
-                
+
                 <button type="button" class="navbar-btn" data-toggle="collapse" data-target="#header-search-form"><i class="fa fa-search"></i></button>
+
                 <div class="navbar-right">
-                    
-                    <nav class="navbar navbar-default">
-                      <div class="container-fluid">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                          </button>
-                        </div>
-                    <?php
-
-                            $defaults = array(
-                                'theme_location'  => 'header',
-                                'container'       => 'div',
-                                'container_class' => 'collapse navbar-collapse',
-                                'container_id' => 'bs-example-navbar-collapse-1',
-                                'menu_class'      => 'nav navbar-nav',
-                                'depth'           => 1,
-                            );
-
-                            wp_nav_menu( $defaults );
-
-                    ?>
-
-                      </div><!-- /.container-fluid -->
-                    </nav>
-
+                  <div id="navbar" class="navbar">
+            				<nav id="top-navigation" class="navigation top-navigation" role="navigation">
+            					<button class="menu-toggle"><?php _e( 'Menu', 'islemag' ); ?></button>
+            					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'islemag' ); ?>"><?php _e( 'Skip to content', 'islemag' ); ?></a>
+            					<?php wp_nav_menu( array( 'theme_location' => 'header', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu', 'depth' => 1 ) ); ?>
+            				</nav><!-- #site-navigation -->
+            			</div><!-- #navbar -->
                 </div>
             </div>
-            
-            
+
             <div class="header-content clearfix">
-                <div class="col-md-3 col-sm-3 col-xs-6 navbar-brand">
+                <div class="col-md-3 col-sm-3 col-xs-12 navbar-brand">
                     <?php
                         global $wp_customize;
                         $islemag_logo = get_theme_mod('islemag_logo');
@@ -100,7 +75,7 @@
                             echo '<div class="header-logo-wrap text-header islemag_only_customizer">';
                             echo '<h1 itemprop="headline" id="site-title" class="site-title"><a href="'.esc_url( home_url( '/' ) ).'" title="'.esc_attr( get_bloginfo( 'name', 'display' ) ).'" rel="home">'.get_bloginfo( 'name' ).'</a></h1>';
                             echo '<p itemprop="description" id="site-description" class="site-description">'.get_bloginfo( 'description' ).'</p>';
-                            echo '</div>';	
+                            echo '</div>';
 
                         else:
 
@@ -117,86 +92,41 @@
                             echo '<p itemprop="description" id="site-description" class="site-description">'.get_bloginfo( 'description' ).'</p>';
                             echo '</div>';
 
-                        endif;	
+                        endif;
                     ?>
                 </div>
 
-                <?php if ( !empty(get_header_image()) ) { ?>
-                <div class="col-md-9 col-sm-9 col-xs-6 islemag-banner">
+                <?php
+                    $header_image = get_header_image();
+                if ( !empty($header_image) ) { ?>
+                <div class="col-md-9 col-sm-9 col-xs-12 islemag-banner">
                     <?php
                         $islemag_banner_link = get_theme_mod('islemag_banner_link','#');
                         if(!empty($islemag_banner_link)){
                             echo '<a href="'.$islemag_banner_link.'">';
-                            echo '<img src="'.get_header_image().'" alt="'.get_bloginfo( 'title' ).'"/>';
+                            echo '<img src="'.$header_image.'" alt="'.get_bloginfo( 'title' ).'"/>';
                             echo '</a>';
                         } else {
-                            echo '<img src="'.get_header_image().'" alt="'.get_bloginfo( 'title' ).'"/>';
+                            echo '<img src="'.$header_image.'" alt="'.get_bloginfo( 'title' ).'"/>';
                         }
                     ?>
                 </div>
                 <?php } ?>
             </div>
-            
-<!-- MENU -->
-					<div itemscope itemtype="http://schema.org/SiteNavigationElement" aria-label="<?php esc_html_e('Primary Menu','islemag') ?>" id="menu-primary" class="navbar-default">
 
-                        <div class="navbar-header">
-                            
-                            
-                          <button id="menu-toggle" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-container" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                          </button>
-                        </div>
-						<!-- LOGO ON STICKY NAV BAR -->
-						<div id="site-header-menu">
-							<nav id="site-navigation" class="main-navigation" role="navigation">
-							<?php
+            <div id="navbar" class="navbar">
+              <nav id="site-navigation" class="navigation main-navigation" role="navigation">
+                <button class="menu-toggle"><?php _e( 'Menu', 'islemag' ); ?></button>
+                <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'islemag' ); ?>"><?php _e( 'Skip to content', 'islemag' ); ?></a>
+                <?php wp_nav_menu( array( 'theme_location' => 'header', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu', 'depth' => 0 ) ); ?>
+              </nav><!-- #site-navigation -->
+            </div><!-- #navbar -->
 
-                                $defaults = array(
-                                    'theme_location'  => 'primary',
-                                    'container'       => 'div',
-                                    'container_class' => 'collapse navbar-collapse',
-                                    'container_id'    => 'main-navbar-container',
-                                    'menu_class'      => 'nav navbar-nav',
-                                    'echo'            => true,
-                                    'fallback_cb'     => 'wp_page_menu',
-                                    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                                    'depth'           => 0,
-                                );
-
-                                wp_nav_menu( $defaults );
-
-                            ?>
-							</nav>
-						</div>
-                    </div>
-                 
-            
-            
-          
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            <div class="collapse navbar-white" id="header-search-form">
-                <form class="navbar-form animated fadeInDown"  role="search">
-                    <input type="search" id="s" name="s" class="form-control" placeholder="Search in here...">
-                </form>
+            <div id="header-search-form">
+              <?php get_search_form(); ?>
             </div><!-- End #header-search-form -->
+
+
         </header><!-- End #header -->
 
 			<div id="content" class="site-content">
--- js/script.all.js
+++ js/script.all.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function() {
-    
-    
-    jQuery("[data-target='#header-search-form']").on('click', function(e) {
-            jQuery('#header-search-form').slideToggle('collapse');
-            e.preventDefault();
+
+
+    jQuery("[data-target='#header-search-form']").on('click', function() {
+            jQuery('#header-search-form').slideDown( "slow", function() {
+              // Animation complete.
+            });
     });
-    
-    
+
+
     jQuery('.owl-carousel.magazine-top-carousel').owlCarousel({
         loop:true,
         margin:0,
@@ -28,7 +29,7 @@ jQuery(document).ready(function() {
             }
         }
     });
-    
+
         /* index23.html - Popular Posts */
     jQuery('.owl-carousel.mpopular-posts').owlCarousel({
         loop:false,
@@ -51,8 +52,8 @@ jQuery(document).ready(function() {
             }
         }
     });
-    
-    
+
+
     /* index23.html - Bigger Carousel */
     jQuery('.owl-carousel.mbigger-posts').owlCarousel({
         loop:false,
@@ -65,8 +66,8 @@ jQuery(document).ready(function() {
         autoplayTimeout: 15000,
         items:1
     });
-    
-    
+
+
     /* index23.html - Most Rated Posts */
     jQuery('.owl-carousel.mmostrated-posts').owlCarousel({
         loop:false,
@@ -89,8 +90,8 @@ jQuery(document).ready(function() {
             }
         }
     });
-    
-    
+
+
     jQuery('.owl-carousel.blog-related-carousel').owlCarousel({
         loop:false,
         margin:15,
@@ -110,70 +111,79 @@ jQuery(document).ready(function() {
             }
         }
     });
-    
+
     jQuery('.related-show-on-click').click(function(){
         jQuery('.islemag-cat-show-on-click').show();
         jQuery(this).hide()
     });
-    
-    
-    
-//ACCESSIBILITY MENU
+
+
+/**
+ * Functionality specific to Twenty Thirteen.
+ *
+ * Provides helper functions to enhance the theme experience.
+ */
+
 ( function( $ ) {
+	var body    = $( 'body' ),
+	    _window = $( window ),
+		nav, button, menu;
 
-    function initMainNavigation( container ) {
-        // Add dropdown toggle that display child menu items.
-        container.find( '.menu-item-has-children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
-
-		// Toggle buttons and submenu items with active children menu items.
-		container.find( '.current-menu-ancestor > button' ).addClass( 'toggled-on' );
-		container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
-
-		// Add menu items with submenus to aria-haspopup="true".
-		container.find( '.menu-item-has-children' ).attr( 'aria-haspopup', 'true' );
-
-		container.find( '.dropdown-toggle' ).click( function( e ) {
-            var _this = $( this );
-            e.preventDefault();
-			_this.toggleClass( 'toggled-on' );
-			_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
-			_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
-			_this.html( _this.html() === screenReaderText.expand ? screenReaderText.collapse : screenReaderText.expand );
-		});
-    }
-    
-    initMainNavigation( $( '.main-navigation' ) );
-    
-    masthead = $( '#masthead' );
-	menuToggle       = masthead.find( '#menu-toggle' );
-	siteHeaderMenu   = masthead.find( '#site-header-menu' );
-	siteNavigation   = masthead.find( '#site-navigation' ); 
-    
-    
-    
-// Enable menuToggle.
-( function() {
-		// Return early if menuToggle is missing.
-        if ( ! menuToggle ) {
-			return;
-		}
-
-		// Add an initial values for the attribute.
-		menuToggle.click(function() {
-			$( this ).add( siteHeaderMenu ).toggleClass( 'toggled-on' );
-		} );
-	} )();
+  button = $( '.menu-toggle' );
+  menu = $( '.nav-menu' );
 
 
-    // Fix sub-menus for touch devices and better focus for hidden submenu items for accessibility.
+  function initMainNavigation( container ) {
+      // Add dropdown toggle that display child menu items.
+      container.find( '.menu-item-has-children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
+      container.find( '.page_item_has_children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
+
+      // Toggle buttons and submenu items with active children menu items.
+      container.find( '.current-menu-ancestor > button' ).addClass( 'toggled-on' );
+      container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
+
+      // Add menu items with submenus to aria-haspopup="true".
+      container.find( '.menu-item-has-children' ).attr( 'aria-haspopup', 'true' );
+
+      container.find( '.dropdown-toggle' ).click( function( e ) {
+              var _this = $( this );
+              e.preventDefault();
+        _this.toggleClass( 'toggled-on' );
+        _this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
+        _this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+        _this.html( _this.html() === screenReaderText.expand ? screenReaderText.collapse : screenReaderText.expand );
+      });
+  }
+
+  initMainNavigation( $( '.main-navigation' ) );
+
+
+
+	/**
+	 * Enables menu toggle for small screens.
+	 */
 	( function() {
-        if ( ! siteNavigation || ! siteNavigation.children().length ) {
-			return;
-		}
 
+
+		button.on( 'click.islemag', function() {
+      nav = $( this ).parent();
+      menu = nav.find( '.nav-menu' );
+
+			nav.toggleClass( 'toggled-on' );
+			if ( nav.hasClass( 'toggled-on' ) ) {
+				$( this ).attr( 'aria-expanded', 'true' );
+				menu.attr( 'aria-expanded', 'true' );
+			} else {
+				$( this ).attr( 'aria-expanded', 'false' );
+				menu.attr( 'aria-expanded', 'false' );
+			}
+		} );
+
+		// Fix sub-menus for touch devices.
 		if ( 'ontouchstart' in window ) {
-			siteNavigation.find( '.menu-item-has-children > a' ).on( 'touchstart.islemag', function( e ) {
+			menu.find( '.menu-item-has-children > a, .page_item_has_children > a' ).on( 'touchstart.islemag', function( e ) {
 				var el = $( this ).parent( 'li' );
+
 				if ( ! el.hasClass( 'focus' ) ) {
 					e.preventDefault();
 					el.toggleClass( 'focus' );
@@ -182,41 +192,56 @@ jQuery(document).ready(function() {
 			} );
 		}
 
-		siteNavigation.find( 'a' ).on( 'focus.islemag blur.islemag', function() {
-			$( this ).parents( '.menu-item' ).toggleClass( 'focus' );
+		// Better focus for hidden submenu items for accessibility.
+		menu.find( 'a' ).on( 'focus.islemag blur.islemag', function() {
+			$( this ).parents( '.menu-item, .page_item' ).toggleClass( 'focus' );
 		} );
 	} )();
 
-
-	// Add he default ARIA attributes for the menu toggle and the navigations.
+	/**
+	 * @summary Add or remove ARIA attributes.
+	 * Uses jQuery's width() function to determine the size of the window and add
+	 * the default ARIA attributes for the menu toggle if it's visible.
+	 * @since Twenty Thirteen 1.5
+	 */
 	function onResizeARIA() {
-		if ( 910 > window.innerWidth ) {
-			if ( menuToggle.hasClass( 'toggled-on' ) ) {
-				menuToggle.attr( 'aria-expanded', 'true' );
-			} else {
-				menuToggle.attr( 'aria-expanded', 'false' );
-			}
-
-			if ( siteHeaderMenu.hasClass( 'toggled-on' ) ) {
-				siteNavigation.attr( 'aria-expanded', 'true' );
-			} else {
-				siteNavigation.attr( 'aria-expanded', 'false' );
-			}
-
-			menuToggle.attr( 'aria-controls', 'site-navigation social-navigation' );
+		if ( 643 > _window.width() ) {
+			button.attr( 'aria-expanded', 'false' );
+			menu.attr( 'aria-expanded', 'false' );
+			button.attr( 'aria-controls', 'primary-menu' );
 		} else {
-			menuToggle.removeAttr( 'aria-expanded' );
-			siteNavigation.removeAttr( 'aria-expanded' );
-			menuToggle.removeAttr( 'aria-controls' );
+			button.removeAttr( 'aria-expanded' );
+			menu.removeAttr( 'aria-expanded' );
+			button.removeAttr( 'aria-controls' );
 		}
 	}
-    
-    $( document ).ready( function() {
-		$( window ).on( 'load.islemag', onResizeARIA )
+
+	_window
+		.on( 'load.islemag', onResizeARIA )
+		.on( 'resize.islemag', function() {
+			onResizeARIA();
 	} );
-    
-    
+
+	/**
+	 * Makes "skip to content" link work correctly in IE9 and Chrome for better
+	 * accessibility.
+	 *
+	 * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+	 */
+	_window.on( 'hashchange.islemag', function() {
+		var element = document.getElementById( location.hash.substring( 1 ) );
+
+		if ( element ) {
+			if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+				element.tabIndex = -1;
+			}
+
+			element.focus();
+		}
+	} );
+
+
 } )( jQuery );
-    
-    
+
+
 });
