90fb32af8af62454f9ab188f6822771eb2304be0
cristian-ungureanu <cristian@themeisle.com>
2017-01-13 12:35:57 +0200

    #216 - remove tgm

--- functions.php
+++ functions.php
@@ -387,48 +387,6 @@ require get_template_directory() . '/inc/jetpack.php';
  */
 require get_template_directory() . '/inc/hooks.php';
 
-/**
- * Include the TGM_Plugin_Activation class.
- */
-require_once get_template_directory() . '/inc/class/class-tgm-plugin-activation.php';
-
-/**
- * Register the required plugins for this theme.
- */
-function islemag_register_required_plugins() {
-	/*
-	 * Array of plugin arrays. Required keys are name and slug.
-	 * If the source is NOT from the .org repo, then source is also required.
-	 */
-	$plugins = array(
-
-		// This is an example of how to include a plugin from the WordPress Plugin Repository.
-		array(
-			'name'      => 'WP Product Review Lite',
-			'slug'      => 'wp-product-review',
-			'required'  => false,
-		),
-
-	);
-
-	/**
-	 * Array of configuration settings. Amend each line as needed.
-	 */
-	$config = array(
-		'id'           => 'islemag',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
-	);
-
-	tgmpa( $plugins, $config );
-}
-add_action( 'tgmpa_register', 'islemag_register_required_plugins' );
-
 
 /**
  * Load customize controls js
