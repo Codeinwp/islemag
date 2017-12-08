1fada69dfe118ac5f0d772120369a8c6feee339a
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-12-08 18:38:41 +0200

    Fixed console error

--- js/script.all.js
+++ js/script.all.js
@@ -194,7 +194,7 @@ jQuery( document ).ready(
 					var element = document.getElementById( location.hash.substring( 1 ) );
 
 					if ( element ) {
-						if ( ! / ^ ( ?: a | select | input | button | textarea)$ / i.test( element.tagName ) ) {
+                        if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
 							element.tabIndex = -1;
 						}
 
